6fbe0b77f3902fd0ea2de32fb790facc7c8953a6
d1mentor <gerasimenkot92@gmail.com>
2021-05-08 18:08:00 +0200

    v 0.1

---
+++ lib/centrobank.rb
@@ -0,0 +1,51 @@
+class Centrobank
+  attr_reader :vaults
+
+  URL = 'https://www.cbr.ru/currency_base/daily/'.freeze
+
+  def initialize(args)
+    @vaults = args
+    @vaults.delete('Букв. код')
+  end
+
+  def self.get_courses
+    html = URI.open(URL)
+    doc = Nokogiri::HTML(html)
+    data = doc.css('.data')
+    tbody = data.xpath('//tbody/tr')
+    args = {}
+
+    tbody.each do |tr|
+      vault_char_code = tr.elements[1].content
+      vault_name = tr.elements[3].content
+      vault_count = tr.elements[2].content.to_i
+      vault_course = tr.elements[4].content.to_f
+
+      #Записываем данные в формате Код_валюты: ['название_валюты', количество, курс]
+      args[vault_char_code] = [vault_name, vault_count, vault_course]
+    end
+
+    new(args)
+  end
+
+  def to_list
+    result_str = ''
+    @vaults.each do |vault_char_code, info|
+      result_str << " #{vault_char_code} - #{info[0]}\n"
+    end
+    return result_str
+  end
+
+  def get_vault_data(vault_char_code)
+    vault_name = @vaults[vault_char_code][0]
+    vault_course = @vaults[vault_char_code][2] / @vaults[vault_char_code][1]
+
+    vault = {
+      "char_code" => vault_char_code,
+      "name" => vault_name,
+      "course" => vault_course.round(2)
+    }
+
+  end
+
+end
---
+++ main.rb
@@ -0,0 +1,62 @@
+if Gem.win_platform?
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+require 'open-uri'
+require 'nokogiri'
+require 'net/http'
+require_relative 'lib/centrobank'
+
+bank = Centrobank::get_courses
+
+puts 'Валюта №1 по умолчанию - RUB'
+puts 'Необходимо выбрать вторую валюту.'
+
+puts bank.to_list
+user_choice = ''
+
+loop do
+  print 'Введите буквенный код валюты:'
+  user_choice = gets.chomp.upcase
+
+  if bank.vaults.key?(user_choice)
+    break
+  else
+    puts 'Некорректный ввод! Попробуйте еще раз.'
+  end
+end
+
+second_vault = bank.get_vault_data(user_choice)
+
+puts "\nПараметры текущего портфеля:"
+puts "Валюты: RUB(рубли) - #{second_vault['char_code']}(#{second_vault['name']})"
+puts "Текущий курс: #{second_vault['course']} RUB = 1 #{second_vault['char_code']}"
+
+print 'Сколько у вас RUB:'
+rub_count = gets.to_i
+print "Сколько у вас #{second_vault['char_code']}:"
+second_vault_count = gets.to_i
+
+rub_in_second_vault = rub_count / second_vault['course']
+second_vault_in_rub = second_vault_count * second_vault['course']
+dif = rub_in_second_vault - second_vault_count
+
+#Проверяем не сбалансирован ли портфель
+if dif.abs <= 0.01
+  puts "Ваш портфель уже сбалансирован"
+#Если Рублей больше
+elsif rub_in_second_vault > second_vault_count
+  #Считаем разницу
+  #Разницу делим на 2 потому что покупаем за свои деньги
+  difference = ((rub_in_second_vault - second_vault_count) / 2).round(2)
+  puts "Вам надо купить #{second_vault['char_code']} #{difference}"
+else
+    #Если второй валюты больше, все аналогично только в другую сторону)
+    difference = ((second_vault_in_rub - rub_count) / 2).round(2)
+    puts "Вам надо купить RUB #{difference}"
+end
